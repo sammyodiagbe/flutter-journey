9d946d4452787f7b3e9e2ce7ebf465d4051f222b
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-08-25 15:44:42 +0100

    learning flutter

--- rida/lib/main.dart
+++ rida/lib/main.dart
@@ -11,19 +11,11 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+
         primarySwatch: Colors.orange,
       ),
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(title: 'Odiagbe Samson Osaro building apps.'),
+      home: MyHomePage(title: 'Puckeet'),
     );
   }
 }
@@ -94,6 +86,20 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            GestureDetector(
+              onTap: _incrementCounter,
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 30),
+                margin: EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5),
+                  color: Colors.lightGreen[500]
+                ),
+                child: Center(child: Text('Tap me fam', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18, color: Colors.white),),),
+              ),
+
+            ),
+            Padding(padding: EdgeInsets.symmetric(vertical: 20),),
             Text(
               'pushed the button this many times:',
             ),
