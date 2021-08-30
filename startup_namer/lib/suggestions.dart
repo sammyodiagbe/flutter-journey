6b40f14ae8a60b5b9e1ef7d053a97a3c543308ce
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-08-30 06:43:15 +0100

    drawer navigator

--- startup_namer/lib/suggestions.dart
+++ startup_namer/lib/suggestions.dart
@@ -18,14 +18,41 @@ class Suggestions extends StatelessWidget {
   final divided = tiles.isNotEmpty ? ListTile.divideTiles(context: context, tiles: tiles).toList() : <Widget>[];
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(icon: Icon(Icons.mail_outline), onPressed: null,),
         title: Text('Your favourites.'),
       ),
       body: ListView(children: divided,),
       drawer: Drawer(
-        child: Center(
-          child: Text('Drawer is that easy'),
+  // Add a ListView to the drawer. This ensures the user can scroll
+  // through the options in the drawer if there isn't enough vertical
+  // space to fit everything.
+  child: ListView(
+    // Important: Remove any padding from the ListView.
+    padding: EdgeInsets.zero,
+    children: [
+      const DrawerHeader(
+        decoration: BoxDecoration(
+          color: Colors.blue,
         ),
+        child: Text('Drawer Header'),
       ),
+      ListTile(
+        title: const Text('Item 1'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+      ListTile(
+        title: const Text('Item 2'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+    ],
+  ),
+),
     );
   }
 }
