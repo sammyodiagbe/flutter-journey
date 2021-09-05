243a5dbf293cab2d0f19cb5671e5bf7c9f2ed96d
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-09-05 09:16:30 +0100

    learning more about layouts

--- piggyvest_clone/lib/screens/home.dart
+++ piggyvest_clone/lib/screens/home.dart
@@ -9,25 +9,33 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    final buildContainer = (double height, Color color) {
-      return Container(
-        height: height,
-        width: MediaQuery.of(context).size.width / 2 + 100,
-        margin: EdgeInsets.only(right: 15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(5),
-          color: color
-        ),
-        );
-       
+    final buildContainer = (Color color, IconData icon, String titleText, String subText) {
+      return GestureDetector(
+          child: Container(
+          alignment: Alignment.centerLeft,
+          child: ListTile(
+            leading: Icon(icon, color: Colors.white,size: 30,),
+            title: Text(titleText, style: TextStyle(color: Colors.white),),
+            subtitle: Text(subText, style: TextStyle(color: Colors.white, fontSize: 25)),
+          ),
+          width: MediaQuery.of(context).size.width / 2 + 100,
+          margin: EdgeInsets.only(right: 15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(5),
+            color: color
+          ),),
+          onTap: () {
+            print(color);
+          }
+          );     
     };
 
     final _titleFontStyle = TextStyle(fontWeight: FontWeight.bold, fontSize: 28);
     final _containerWidgets = <Widget>[
-      buildContainer(60, Colors.indigo),
-      buildContainer(60, Colors.purple),
-      buildContainer(60, Colors.pink),
-      buildContainer(60, Colors.orange),
+      buildContainer(Colors.blue.shade900, Icons.shield_outlined, 'Total Savings', '\$23,000'),
+      buildContainer(Colors.purpleAccent, Icons.analytics, 'Total Investments', '\$3000'),
+      buildContainer(Colors.black, Icons.money,'Flex Dollar', '\$10000'),
+      buildContainer(Colors.grey.shade400, Icons.video_camera_back, 'Flex Naira', '\$200'),
     ];
     return Scaffold(
       
