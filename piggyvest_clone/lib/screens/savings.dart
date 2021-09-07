0550320c7cbbe238f3047c957cd5131bf79d642e
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-09-07 20:25:37 +0100

    gotten it

--- piggyvest_clone/lib/screens/savings.dart
+++ piggyvest_clone/lib/screens/savings.dart
@@ -8,12 +8,12 @@ class Savings extends StatelessWidget {
     final _gridItem = (Color color, IconData icon, String title, String subtitle, String price) {
       return Container(
         padding: EdgeInsets.all(15),
-        height: 150,
         decoration: BoxDecoration(
           color: color,
           borderRadius: BorderRadius.circular(8)
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Icon(icon),
             SizedBox(height: 20),
@@ -31,6 +31,7 @@ class Savings extends StatelessWidget {
       body: Container(
         padding: EdgeInsets.only(top: 30, left: 15, right: 15),
         height: double.infinity,
+        
         child: ListView(
           shrinkWrap: true,
           children: [
@@ -40,24 +41,49 @@ class Savings extends StatelessWidget {
               subtitle: Text('\$28,000', style: TextStyle(fontSize:18, color: Colors.indigo, fontWeight: FontWeight.bold )),
             ),
             SizedBox(
-              height: 50
+              height: 30
             ),
+
             Container(
-              height: 300,
-              child: GridView.count(
-                shrinkWrap: true,
+              child: Container(
+                child: Row(
+                      children: [
+                         Expanded(child: _gridItem(Colors.blue.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200')),
+                         SizedBox(width: 20,),
+                          Expanded(child: _gridItem(Colors.pink.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200')),
+                      ],
+                    ),
+              ),
+            ), 
+            SizedBox(height: 20),
+            Container(
+              child: Container(
+                child: Row(
+                      children: [
+                         Expanded(child: _gridItem(Colors.grey.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200')),
+                         SizedBox(width: 20,),
+                          Expanded(child: _gridItem(Colors.green.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200')),
+                      ],
+                    ),
+              ),
+            ),
+            // Container(
+            //   height: 300,
+            //   color: Colors.cyan,
+            //   child: GridView.count(
+            //     shrinkWrap: true,
                 
-                crossAxisCount: 2,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 20,
-                children: [
-                  _gridItem(Colors.blue.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
-                  _gridItem(Colors.pink.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
-                  _gridItem(Colors.grey.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
-                  _gridItem(Colors.green.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
+            //     crossAxisCount: 2,
+            //     mainAxisSpacing: 20,
+            //     crossAxisSpacing: 20,
+            //     children: [
+            //       _gridItem(Colors.blue.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
+            //       _gridItem(Colors.pink.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
+            //       _gridItem(Colors.grey.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
+            //       _gridItem(Colors.green.shade100,Icons.shield_outlined,'Piggybank', 'Strict saving automatically weekly or daily', '1200'),
                   
-              ],),
-            )
+            //   ],),
+            // )
           ],
         )
       ),
