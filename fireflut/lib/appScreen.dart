f80cd9b29781ed1b8a3960e3a27d6431d411ee20
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-09-13 21:37:17 +0100

    realtime data

--- fireflut/lib/appScreen.dart
+++ fireflut/lib/appScreen.dart
@@ -11,24 +11,46 @@ class AppScreen extends StatefulWidget {
 class _AppScreenState extends State<AppScreen> {
 
   CollectionReference _talks = FirebaseFirestore.instance.collection('talks');
+  Stream<QuerySnapshot> _stream = FirebaseFirestore.instance.collection('talks').snapshots();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Center(
-        child: ElevatedButton(
-          child: Text('I dare you to press me'),
-          onPressed: () {
-            _talks.add(
-              {
-                'talk': 'Some random talks right here',
-                'bywhom': true
+      child: StreamBuilder<QuerySnapshot> (
+            stream: _stream,
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot ){
+              if(snapshot.hasError) {
+                print('Something went wrong');
               }
-            ).then((value) => print('User added'))
-            .catchError((error) => print('There was an error $error'));
-          },
-        ),
-      )
+
+              
+                return ListView(
+                  shrinkWrap: true,
+                  children: snapshot.data!.docs.map((document) {
+                    Map<String, dynamic> data = document.data()! as Map<String, dynamic>;
+                    print(data);
+                    return ListTile(
+                      title: data['bywhom'] ? Text('yes') : Text('no'),
+                      subtitle: Text(data['talk']),
+                    );
+                  }).toList(),
+                );
+              
+            },
+          ),
+        //   ElevatedButton(
+        //   child: Text('I dare you to press me'),
+        //   onPressed: () {
+        //     _talks.add(
+        //       {
+        //         'talk': 'Some random talks right here',
+        //         'bywhom': true
+        //       }
+        //     ).then((value) => print('User added'))
+        //     .catchError((error) => print('There was an error $error'));
+        //   },
+        // )],
+      
     );
   }
 }
