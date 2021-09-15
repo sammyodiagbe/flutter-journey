6f380bc147aef58932eed2525deb3f4aed0b439c
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-09-15 05:20:57 +0100

    adjusting text theme

--- fireflut/lib/main.dart
+++ fireflut/lib/main.dart
@@ -34,12 +34,13 @@ class _SpellBattleState extends State<SpellBattle> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.rubikTextTheme(),
+        textTheme: GoogleFonts.alataTextTheme(),
         primaryColor: Color(0xff1E3163),
         iconTheme: IconThemeData(
           size: 20
         ),
-        canvasColor: Color(0xff1E3163),
+        // canvasColor: Color(0xff1E3163),
+        brightness: Brightness.dark,
         buttonTheme: ButtonThemeData(
           buttonColor: Color(0xff28FFBF),
         )
