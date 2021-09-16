2dbfa84bc3bbc153af1792e9ee2092d5ccf890d5
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-09-16 10:47:43 +0100

    fixing form fields error

--- fireflut/lib/screens/loginScreen.dart
+++ fireflut/lib/screens/loginScreen.dart
@@ -17,100 +17,103 @@ class _LoginState extends State<Login> {
       body: Container(
         height: double.infinity,
         padding: EdgeInsets.all(19),
-        child: ListView(
-          
-          children: [
-            SizedBox(
-              height: 80,
-              
-              child: Center(
-                child: Text('Spellbatu', style: loginHeaderSize,)
-                )
-              ),
-            SizedBox(
-              height: 100,
-              
-              child: Center(
-                child: Text('Login into your account', style: loginSignupHeader)
-                )
-              ),
-              Container(
-                margin: EdgeInsets.only(bottom: 20),
-                child: Form(
-                  key: _loginformKey,
-                  // autovalidateMode: AutovalidateMode.always,
-                  child: Column(
 
-                    children: [
-                      SizedBox(
-                        height: 100,
-                        child: TextFormField(
-                          keyboardType: TextInputType.emailAddress,
-                          style: TextStyle(color: Colors.white),
-                          validator: (value) {
-                            if(value == null || value.isEmpty) {
-                              return '*Please enter some text.';
-                            }
-                            return null;
-                          },
-                          decoration: InputDecoration(
-                            hintStyle: TextStyle(color: Colors.white60),
-                            border: OutlineInputBorder(),
-                            hintText: 'Email address',
-                            focusColor: Colors.white,
-                          )
-                        ),
-                      ),
-                     TextFormField(
-                          keyboardType: TextInputType.text,
-                          obscureText: true,
-                          validator: (value) {
-                            if(value == null || value.isEmpty) {
-                              return '*Please enter some text.';
-                            }
-                            return null;
-                          },
-                          style: TextStyle(color: Colors.white),
-                          decoration: InputDecoration(
-                            hintStyle: TextStyle(color: Colors.white60),
-                            
-                            border: OutlineInputBorder(),
-                            hintText: 'Password',
-                            focusColor: Colors.white
-                            
-                          )
-                        ),
-                        SizedBox(height: 50),
-                       Container(
-                         child: Row(
-                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                           children: [
-                             Container(
-                                child: ElevatedButton(
-                                  child: Text('Send me in', style: TextStyle(fontWeight: FontWeight.bold),),
-                                  onPressed: (){
-                                    // if(_loginformKey.currentState!.)
-                                    // if(_loginformKey.currentState!.validate())
-                                  },
-                                  ),
-                              ),
-                              OutlinedButton(
-                                onPressed: (){
-                                  Navigator.of(context).pushReplacementNamed("signup");
-                                }, 
-                                child: Text('Create an account', style: TextStyle(color: Colors.white)
-                                ),
-                                ),
-                           ],
-                         ),
-                       )
-                    ],
-
+        child: SingleChildScrollView(
+          child: Column(
+            
+            children: [
+              SizedBox(
+                height: 80,
+                
+                child: Center(
+                  child: Text('Spellbatu', style: loginHeaderSize,)
                   )
-                )
-              ),
-              
-          ],
+                ),
+              SizedBox(
+                height: 100,
+                
+                child: Center(
+                  child: Text('Login into your account', style: loginSignupHeader)
+                  )
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: Form(
+                    key: _loginformKey,
+                    // autovalidateMode: AutovalidateMode.always,
+                    child: Column(
+        
+                      children: [
+                        SizedBox(
+                          height: 100,
+                          child: TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            style: TextStyle(color: Colors.white),
+                            validator: (value) {
+                              if(value == null || value.isEmpty) {
+                                return '*Please enter some text.';
+                              }
+                              return null;
+                            },
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white60),
+                              border: OutlineInputBorder(),
+                              hintText: 'Email address',
+                              focusColor: Colors.white,
+                            )
+                          ),
+                        ),
+                       TextFormField(
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                            validator: (value) {
+                              if(value == null || value.isEmpty) {
+                                return '*Please enter some text.';
+                              }
+                              return null;
+                            },
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintStyle: TextStyle(color: Colors.white60),
+                              
+                              border: OutlineInputBorder(),
+                              hintText: 'Password',
+                              focusColor: Colors.white
+                              
+                            )
+                          ),
+                          SizedBox(height: 50),
+                         Container(
+                           child: Row(
+                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                             children: [
+                               Container(
+                                  child: ElevatedButton(
+                                    child: Text('Send me in', style: TextStyle(fontWeight: FontWeight.bold),),
+                                    onPressed: (){
+                                      // if(_loginformKey.currentState!.)
+                                      // if(_loginformKey.currentState!.validate())
+                                    },
+                                    ),
+                                ),
+                                OutlinedButton(
+                                  onPressed: (){
+                                    Navigator.of(context).pushReplacementNamed("signup");
+                                  }, 
+                                  child: Text('Create an account', style: TextStyle(color: Colors.white)
+                                  ),
+                                  ),
+                             ],
+                           ),
+                         )
+                      ],
+        
+                    )
+                  )
+                ),
+                
+            ],
+          ),
         ),
         )
     );
