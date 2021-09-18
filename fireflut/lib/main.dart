c0b8f6b072559a25101b1406cd793d2ef989d710
odiagbe samson osaro <odiagbesamsonosaro@gmail.com>
2021-09-18 09:44:20 +0100

    fixing app structure

--- fireflut/lib/main.dart
+++ fireflut/lib/main.dart
@@ -2,8 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:fireflut/screens/AuthenticationScreenManger.dart';
 import 'package:fireflut/screens/Battle.dart';
 import 'package:fireflut/screens/authStateCheck.dart';
+import 'package:fireflut/screens/loginScreen.dart';
+import 'package:fireflut/screens/signupScreen.dart';
 import 'package:fireflut/utils/AuthProvider.dart';
 import "package:flutter/material.dart";
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
 
@@ -32,45 +35,66 @@ class _SpellBattleState extends State<SpellBattle> {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      
-      builder: (context,snapshot){
-        if(snapshot.connectionState == ConnectionState.done) {
-          return Consumer<AuthProvider>(
-          builder: (context, appState, _) {
-            if(appState.getState == AuthenticationState.VerifyingAuthState) {
-              return AuthStateDecider();
-            }
-             if(appState.getState == AuthenticationState.Authenticated) {
-              return BatuGround();
-            }
-            if(appState.getState == AuthenticationState.Unauthenticated) {
-              return AuthWrapper();
-            }
-           
-            return Container(
-              height: double.infinity,
-              child: Center(
-                child: Text('BrainBatu', 
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 32
-                 ),
-                 ),
-              ),
-            );          
-          },
-        );
-      
-      
+    return MaterialApp(
+      routes: {
+        'login': (context) => Login(),
+        'signup': (context) => Signup()
+      },
+      theme: ThemeData(
+        textTheme: GoogleFonts.alataTextTheme(),
+        primaryColor: Color(0xff1E3163),
+        iconTheme: IconThemeData(
+          size: 20
+        ),
+        canvasColor: Color(0xff1E3163),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Color(0xff57CC99),
+            padding: EdgeInsets.all(15)
+          )
+        ),
+        
+      ),
+      home: FutureBuilder(
+        future: _initialization,
+        
+        builder: (context,snapshot){
+          if(snapshot.connectionState == ConnectionState.done) {
+            return Consumer<AuthProvider>(
+            builder: (context, appState, _) {
+              if(appState.getState == AuthenticationState.VerifyingAuthState) {
+                return AuthStateDecider();
+              }
+               if(appState.getState == AuthenticationState.Authenticated) {
+                return BatuGround();
+              }
+              if(appState.getState == AuthenticationState.Unauthenticated) {
+                return AuthWrapper();
+              }
+             
+              return Container(
+                height: double.infinity,
+                child: Center(
+                  child: Text('BrainBatu', 
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 32
+                   ),
+                   ),
+                ),
+              );          
+            },
+          );
+        
+        
+          }
+          return Container(
+            child: Center(
+              child: Text('Brainbatu', style: TextStyle(color: Colors.white, fontSize: 35),),
+            ),
+          );
         }
-        return Container(
-          child: Center(
-            child: Text('Brainbatu', style: TextStyle(color: Colors.white, fontSize: 35),),
-          ),
-        );
-      }
+      ),
     );
     
   }
